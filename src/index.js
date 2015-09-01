a0fbf10b03d0387829ea60256c3a7828daddd417
Burak Can <neoberg@gmail.com>
2015-09-01 14:30:13 +0300

    Fixed: Use sticky-kit for search box

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import 'babel-core/polyfill';
+import sticky from 'root/vendor/jquery.sticky-kit.min.js';
 import React from 'react';
 import App from 'containers/App';
 import 'styles/main';
+
+sticky.call(window);
 
 React.render(
   <App/>, document.getElementById('root')
